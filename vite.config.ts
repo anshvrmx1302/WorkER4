b01cd9f2e93505d0106ec6b2cdb9a938647c81ab
Ansh Kumar Verma <anshvermaiit@gmail.com>
2025-12-28 17:58:56 +0530

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { createServer } from "./server";
 
 export default defineConfig(({ mode }) => ({
   server: {
-    host: "::",
+    host: "0.0.0.0",
     port: 8080,
     fs: {
       // 👇 Add project root to allow list
